7c047efd08ff990d4b66994c9833daedf9316e9b
xujoe <jxujoe@163.com>
2020-01-03 10:18:46 +0800

    完成master

---
+++ Modbus_Master/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Modbus_Master
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.txt_show = new System.Windows.Forms.TextBox();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_SlaveID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_FunCode = new System.Windows.Forms.ComboBox();
+            this.textBox_startAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_numInputs = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel_meg = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel_errImage = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel_err = new System.Windows.Forms.ToolStripLabel();
+            this.btn_Read = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.btn_Write = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(47, 40);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "连接";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // txt_show
+            // 
+            this.txt_show.Location = new System.Drawing.Point(252, 7);
+            this.txt_show.Multiline = true;
+            this.txt_show.Name = "txt_show";
+            this.txt_show.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_show.Size = new System.Drawing.Size(358, 193);
+            this.txt_show.TabIndex = 1;
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(35, 13);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(112, 20);
+            this.txt_ip.TabIndex = 2;
+            this.txt_ip.Text = "127.0.0.1";
+            // 
+            // txt_port
+            // 
+            this.txt_port.Location = new System.Drawing.Point(186, 13);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(45, 20);
+            this.txt_port.TabIndex = 3;
+            this.txt_port.Text = "502";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "从站号";
+            // 
+            // textBox_SlaveID
+            // 
+            this.textBox_SlaveID.Location = new System.Drawing.Point(77, 115);
+            this.textBox_SlaveID.Name = "textBox_SlaveID";
+            this.textBox_SlaveID.Size = new System.Drawing.Size(45, 20);
+            this.textBox_SlaveID.TabIndex = 7;
+            this.textBox_SlaveID.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "功能码";
+            // 
+            // comboBox_FunCode
+            // 
+            this.comboBox_FunCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_FunCode.Items.AddRange(new object[] {
+            "01 Read Coils (0x)",
+            "02 Read Discrete Inputs (1x)",
+            "03 Read Holding Registers [4x]",
+            "04 Read Input Registers [3x]"});
+            this.comboBox_FunCode.Location = new System.Drawing.Point(77, 152);
+            this.comboBox_FunCode.Name = "comboBox_FunCode";
+            this.comboBox_FunCode.Size = new System.Drawing.Size(153, 21);
+            this.comboBox_FunCode.TabIndex = 9;
+            // 
+            // textBox_startAddress
+            // 
+            this.textBox_startAddress.Location = new System.Drawing.Point(77, 183);
+            this.textBox_startAddress.Name = "textBox_startAddress";
+            this.textBox_startAddress.Size = new System.Drawing.Size(45, 20);
+            this.textBox_startAddress.TabIndex = 11;
+            this.textBox_startAddress.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "起始地址";
+            // 
+            // textBox_numInputs
+            // 
+            this.textBox_numInputs.Location = new System.Drawing.Point(186, 183);
+            this.textBox_numInputs.Name = "textBox_numInputs";
+            this.textBox_numInputs.Size = new System.Drawing.Size(45, 20);
+            this.textBox_numInputs.TabIndex = 13;
+            this.textBox_numInputs.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(128, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "地址长度";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.toolStripSeparator1,
+            this.toolStripLabel_meg,
+            this.toolStripLabel_errImage,
+            this.toolStripLabel_err});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 638);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(614, 25);
+            this.toolStrip1.TabIndex = 15;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.AutoSize = false;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(22, 22);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel_meg
+            // 
+            this.toolStripLabel_meg.AutoSize = false;
+            this.toolStripLabel_meg.Name = "toolStripLabel_meg";
+            this.toolStripLabel_meg.Size = new System.Drawing.Size(180, 22);
+            this.toolStripLabel_meg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripLabel_errImage
+            // 
+            this.toolStripLabel_errImage.AutoSize = false;
+            this.toolStripLabel_errImage.Name = "toolStripLabel_errImage";
+            this.toolStripLabel_errImage.Size = new System.Drawing.Size(22, 22);
+            // 
+            // toolStripLabel_err
+            // 
+            this.toolStripLabel_err.AutoSize = false;
+            this.toolStripLabel_err.Name = "toolStripLabel_err";
+            this.toolStripLabel_err.Size = new System.Drawing.Size(340, 22);
+            this.toolStripLabel_err.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_Read
+            // 
+            this.btn_Read.Location = new System.Drawing.Point(128, 40);
+            this.btn_Read.Name = "btn_Read";
+            this.btn_Read.Size = new System.Drawing.Size(75, 23);
+            this.btn_Read.TabIndex = 16;
+            this.btn_Read.Text = "读取";
+            this.btn_Read.UseVisualStyleBackColor = true;
+            this.btn_Read.Click += new System.EventHandler(this.btn_Read_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(47, 69);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(75, 23);
+            this.btn_disconnect.TabIndex = 17;
+            this.btn_disconnect.Text = "断开";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // btn_Write
+            // 
+            this.btn_Write.Location = new System.Drawing.Point(128, 69);
+            this.btn_Write.Name = "btn_Write";
+            this.btn_Write.Size = new System.Drawing.Size(75, 23);
+            this.btn_Write.TabIndex = 18;
+            this.btn_Write.Text = "写入";
+            this.btn_Write.UseVisualStyleBackColor = true;
+            this.btn_Write.Click += new System.EventHandler(this.btn_Write_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(598, 402);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 663);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Write);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_Read);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBox_numInputs);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_startAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox_FunCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_SlaveID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_port);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.txt_show);
+            this.Controls.Add(this.btn_connect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Modbus-Master";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox txt_show;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_SlaveID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_FunCode;
+        private System.Windows.Forms.TextBox textBox_startAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_numInputs;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel_meg;
+        private System.Windows.Forms.Button btn_Read;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Button btn_Write;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel_err;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel_errImage;
+    }
+}
+
---
+++ Modbus_Master/Form1.cs
@@ -0,0 +1,230 @@
+﻿using Modbus.Device;
+using System;
+using System.Data;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace Modbus_Master
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox_FunCode.SelectedIndex = 0;
+            txt_ip.Text = "127.0.0.1";
+            txt_port.Text = "502";
+            textBox_SlaveID.Text = "1";
+            textBox_startAddress.Text = "0";
+            textBox_numInputs.Text = "10";
+
+
+            btn_connect.PerformClick();
+        }
+        ModbusIpMaster master;
+        ushort startAddress = 0;
+        ushort numInputs = 10;
+        byte slaveID = 1;
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            Error(false);
+            try
+            {
+                TcpClient client = new TcpClient(txt_ip.Text.Trim().ToString(), int.Parse(txt_port.Text.Trim().ToString()));
+                client.SendTimeout = 1;
+                master = ModbusIpMaster.CreateIp(client);
+                if (master != null)
+                {
+                    toolStripLabel_meg.Text = $"{txt_ip.Text}:{txt_port.Text}连接成功！";
+                    toolStripLabel2.Image = global::Modbus_Master.Properties.Resources.连接成功;
+                }
+
+                btn_disconnect.Enabled = true;
+                btn_connect.Enabled = false;
+            }
+            catch (Exception eee)
+            {
+                Error(true, eee);
+            }
+        }
+
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+
+            master.Dispose();
+
+            if (master.Transport == null)
+            {
+                toolStripLabel_meg.Text = $"{txt_ip.Text}：{txt_port.Text}连接已断开！";
+                toolStripLabel2.Image = global::Modbus_Master.Properties.Resources.连接断开;
+            }
+            btn_disconnect.Enabled = false ;
+            btn_connect.Enabled = true;
+        }
+
+        private void btn_Read_Click(object sender, EventArgs e)
+        {
+
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("地址", typeof(int));
+            dt.Columns.Add("值", typeof(ushort));
+
+
+
+            ushort[] datas = ModbusRead();
+            if (datas != null)
+            {
+                for (int i = 0; i < datas.Length; i++)
+                {
+
+                    dt.Rows.Add(new object[] { i + startAddress, datas[i] });
+
+                }
+            }
+
+            dataGridView1.DataSource = dt;
+
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = false;
+        }
+
+        private void btn_Write_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private ushort[] ModbusRead()
+        {
+            Error(false);
+            try
+            {
+                startAddress = ushort.Parse(textBox_startAddress.Text);
+                numInputs = ushort.Parse(textBox_numInputs.Text);
+                slaveID = byte.Parse(textBox_SlaveID.Text);
+                ushort[] datas = new ushort[numInputs];
+               
+
+                switch (comboBox_FunCode.SelectedIndex)
+                {
+                    case 0:
+                        bool[] coils = master.ReadCoils(slaveID, startAddress, numInputs);
+                        for (int i = 0; i < numInputs; i++)
+                        {
+                            datas[i] = (ushort)(coils[i] ? 1 : 0);
+                        }
+
+                        break;
+                    case 1:
+
+                        bool[] inputs = master.ReadInputs(slaveID, startAddress, numInputs);
+                        for (int i = 0; i < numInputs; i++)
+                        {
+                            datas[i] = (ushort)(inputs[i] ? 1 : 0);
+                        }
+                        break;
+                    case 2:
+                        ushort[] holdingRegisters = master.ReadHoldingRegisters(slaveID, startAddress, numInputs);
+                        datas = holdingRegisters;
+                        break;
+                    case 3:
+                        ushort[] inputRegisters = master.ReadInputRegisters(slaveID, startAddress, numInputs);
+                        datas = inputRegisters;
+                        break;
+                    default:
+                        MessageBox.Show("功能码错误");
+                        datas = null;
+                        break;
+                }
+                txt_show.AppendText("\r\nModbusRead Success");
+                return datas;
+            }
+            catch (Exception eee)
+            {
+                Error(true, eee);
+                return null;
+            }
+        }
+
+
+
+        private void Error(bool IsError, Exception eee = null)
+        {
+            if (!IsError)
+            {
+                toolStripLabel_errImage.Image = null;
+                toolStripLabel_err.Text = null;
+            }
+            else
+            {
+                toolStripLabel_errImage.Image = global::Modbus_Master.Properties.Resources.连接失败;
+                toolStripLabel_err.Text = eee.Message;
+            }
+        }
+
+        private void Error(string eee = null)
+        {
+
+            toolStripLabel_errImage.Image = global::Modbus_Master.Properties.Resources.连接失败;
+            toolStripLabel_err.Text = eee;
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            Error(e.RowIndex + "-" + e.ColumnIndex  );
+            ushort Address = ushort.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex - 1].Value.ToString());
+            ushort Value = ushort.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+            ModbusWrite(Address, Value);
+
+
+
+        }
+
+
+        private void ModbusWrite(ushort Address, ushort Value)
+        {
+            Error(false);
+            try
+            {
+               
+                slaveID = byte.Parse(textBox_SlaveID.Text);
+                ushort[] datas = new ushort[numInputs];
+               
+
+                switch (comboBox_FunCode.SelectedIndex)
+                {
+                    case 0:
+                         master.WriteSingleCoil(slaveID, Address, Value==0?false:true);
+                        break;
+                    case 1:
+                        master.WriteSingleCoil(slaveID, Address, Value == 0 ? false : true);
+                        break;
+                    case 2:
+                        master.WriteSingleRegister(slaveID, Address, Value);
+                        break;
+                    case 3:
+                        master.WriteSingleRegister(slaveID, Address, Value);
+                        break;
+                    default:
+                        MessageBox.Show("功能码错误");
+                        datas = null;
+                        break;
+                }
+                txt_show.AppendText("\r\nModbusWrite Success");
+              
+            }
+            catch (Exception eee)
+            {
+                Error(true, eee);
+            }
+        }
+
+    }
+}
